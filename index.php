63175e1f6bcfde6ec8ca5da89f71a3ed760c3317
SniperMonkey <89995943+IsaacDeLeonCarbajal@users.noreply.github.com>
2024-05-22 13:02:40 -0600

    feaet: index with encrypt example

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Vigenere\Support\GlobalFunctionsProvider;
+use Vigenere\VigenereCipher;
+
+(new GlobalFunctionsProvider)(); // Register helper functions
+
+$obj = new VigenereCipher(
+    transform: fn ($v) => strtoupper($v),
+);
+
+echo $obj->encrypt('vERSAILLES', 'CHEESE');
